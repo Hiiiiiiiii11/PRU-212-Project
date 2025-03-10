7be725b2f08143ccb0c5510c8a6505ad395640ed
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-03-10 13:04:43 +0700

    Update Audio Manager

--- Assets/Level 3/Script/AudioManager.cs
+++ Assets/Level 3/Script/AudioManager.cs
@@ -4,10 +4,26 @@ namespace level3
 {
     public class AudioManager : MonoBehaviour
     {
+        public static AudioManager instance;
         public Sound[] musicSounds, sfxSounds;
         public AudioSource musicSource, sfxSource;
 
-        public void PlayMusic()
+        private void Awake()
+        {
+            if(instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else Destroy(gameObject);
+        }
+
+        private void Start()
+        {
+            PlayMusic("Theme");
+        }
+
+        public void PlayMusic(string name)
         {
             Sound s = Array.Find(musicSounds, x => x.name == name);
             if (s == null)
