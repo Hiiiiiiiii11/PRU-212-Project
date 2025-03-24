e3d96b3961e5163c33bc6427402c825e420bbcb3
QuocHuy <huyhqse173627@fpt.edu.vn>
2025-03-25 03:48:26 +0700

    Revert "Merge branch 'main' of https://github.com/Hiiiiiiiii11/PRU-212-Project into Huy"
    This reverts commit 22cb0803b46b8bf7ea3f76a859a6727339a585f1, reversing
    changes made to 8f872deb17297a057dbb3ab288dfd766afc5c676.

--- Assets/Level 2/Scripts/PlayerController.cs
+++ Assets/Level 2/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+
 namespace level2
 {
     public class PlayerController : MonoBehaviour
@@ -8,57 +9,47 @@ namespace level2
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private Transform groundCheck;
         private Animator animator;
-
-        private bool isGrounded;
-
         private Rigidbody2D rb;
+        private bool isGrounded;
 
         private void Awake()
         {
             rb = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
         }
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
 
-        }
-
-        // Update is called once per frame
         void Update()
         {
             HandleMovement();
             HandleJump();
             UpdateAnimation();
         }
+
         private void HandleMovement()
         {
             float moveInput = Input.GetAxis("Horizontal");
-
             rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-            Vector3 currentScale = transform.localScale;
-
-            if (moveInput > 0 && currentScale.x < 0) transform.localScale = new Vector3(Mathf.Abs(currentScale.x), currentScale.y, currentScale.z);
-
-            else if (moveInput < 0 && currentScale.x > 0) transform.localScale = new Vector3(-Mathf.Abs(currentScale.x), currentScale.y, currentScale.z);
+            if (moveInput != 0)
+            {
+                transform.localScale = new Vector3(Mathf.Sign(moveInput) * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
         }
 
         private void HandleJump()
         {
-            if (Input.GetButtonDown("Jump") && isGrounded)
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+            if (isGrounded && Input.GetButtonDown("Jump"))
             {
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
             }
-            isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
         }
 
         private void UpdateAnimation()
         {
-            float isRunning = Mathf.Abs(rb.linearVelocity.x);
-            bool isJumping = !isGrounded;
-            animator.SetFloat("Speed", isRunning);
-            animator.SetBool("Grounded", !isJumping);
+            animator.SetFloat("Speed", Mathf.Abs(rb.linearVelocity.x));
+            animator.SetBool("Grounded", isGrounded);
         }
     }
 }
